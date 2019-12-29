a1adb2f25c5fed7f9678c0ac783a617104664ce9
Olakunle Boye <kunlexzy@gmail.com>
2019-12-29 23:58:27 +0100

    add Coinbase api, working on left side tab :pencil:

---
+++ src/components/LeftTab.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+  <div class="Tabs" role="tabpanel">
+    <div class="Tabs-item"
+          v-for="(data, index) in itemData" :key="index"
+          :class="(index === selectedCryptocurrencyIndex) && 'selected'">
+      <span v-if="spotPrices[0]" class="cryptocurrency"
+            @click="$emit('triggerCurrency', index, data, spotPrices)">
+        <span>{{ data.name }}</span>
+        <span>{{ spotPrices[index].amount }}</span>
+      </span>
+      <span v-else @click="$emit('triggerCurrency', data)" class="cryptocurrency"> <span>{{ data.name }}</span></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    itemData: {
+      type: Array
+    },
+    spotPrices: {
+      type: Array
+    }
+  },
+  data() {
+    return {
+      selectedCryptocurrencyIndex: 0
+    }
+  }
+}
+</script>
