ef56e8d094559eea01c87b30495b7cde73d4cafc
Olakunle Boye <kunlexzy@gmail.com>
2019-12-30 10:05:24 +0100

    Working on both right and left tabs :wrench:

--- src/components/RightTab.vue
+++ src/components/RightTab.vue
@@ -1,7 +1,12 @@
 <template lang="html">
   <div class="Tabs" role="tabpanel">
     <div class="Tabs-item"
-          v-for="(data, index) in itemData">
+          v-for="(data, index) in itemData"
+          :aria-labelledby="data.codename"
+          tabindex="-1"
+          @click="$emit('triggerDuration', index, data)"
+          :class="(index === selectedDurationIndex) && 'selected'">
+      <span>{{ data.codename }}</span>
 
     </div>
   </div>
@@ -12,6 +17,15 @@ export default {
   props: {
     itemData: {
       type: Array
+    },
+      selectedDurationIndex: {
+        type: Number,
+        default: 2
+      }
+  },
+  data() {
+    return {
+
     }
   }
 }
